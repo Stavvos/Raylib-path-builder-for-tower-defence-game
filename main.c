7eb6af3dd4631325fd1d6407155124303660dfc6
liam henry <liam_burridgeuc@hotmail.com>
2026-01-30 15:37:17 +1100

    first commit

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "raylib.h"
+
+int main(void)
+{
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Level Editor");
+
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+    SetTargetFPS(60);                  
+
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
+    {
+        //Update
+        UpdateCamera(&camera, CAMERA_FREE);
+
+        BeginDrawing();
+          ClearBackground(RAYWHITE);
+        
+	//render 3D goes here
+          BeginMode3D(camera);
+            DrawGrid(10, 1.0f);
+          EndMode3D();
+
+        //render 2D goes here
+	
+        EndDrawing();
+    }
+
+    CloseWindow();        // Close window and OpenGL context
+
+    return 0;
+}
+
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm ./levelEditor
+
+if gcc -Wall -pedantic -o levelEditor main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11; then
+  echo compiled
+else
+  exit $?
+fi
+
+if test "$1" != check ; then
+  ./levelEditor
+fi
