c5432f28d68a86182a302a5a866afd69a56c809e
liam henry <liam_burridgeuc@hotmail.com>
2026-02-09 21:26:04 +1100

    NEWFEATURE: There is now a terrain model which is generated from building a mesh via heightMap.png

--- initialise.c
+++ initialise.c
@@ -1,21 +1,26 @@
 #include "utils.h"
 
-void initLevelPoints(struct Point levelPoints[ROWS][COLS])
+void initLevelPoints(struct Point levelPoints[ROWS][COLS], Mesh level, Matrix transform)
 {
   int spacing = 6;
   int X = -30.0f;
-  int Y = -30.0f;
+  int Z = -30.0f;
+  
+  Ray ray;
+  ray.direction = (Vector3){0.0f,1.0f,0.0f};
 
   for (int i = 0; i < ROWS; i++)
   {
     for (int j = 0; j < COLS; j++)
     {
-      levelPoints[i][j].pos = (Vector3){X, 0.0, Y};
+      ray.position = (Vector3){X, 0.0f, Z};
+      RayCollision hit = GetRayCollisionMesh(ray, level, transform);
+      levelPoints[i][j].pos = (Vector3){X, hit.point.y, Z};
       levelPoints[i][j].pointState = NOPOINT;
       levelPoints[i][j].direction = NODIRECTION;
       X += spacing;
     }
     X = -30.0f;
-    Y += spacing;
+    Z += spacing;
   }
 }
--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "GUI.c"
 #include "camera.c"
 #include "path.c"
+#include "raymath.h"
 
 int main(void)
 {
@@ -16,24 +17,39 @@ int main(void)
 
   // Define the camera to look into our 3d world
   Camera3D camera = { 0 };
-  camera.position = (Vector3){ 0.0f, 90.0f, 0.0f }; // Camera position
+  camera.position = (Vector3){ 50.0f, 50.0f, 50.0f }; // Camera position
   camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-  camera.up = (Vector3){ 0.0f, 0.0f, -1.0f };          // Camera up vector (rotation towards target)
+  camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
   camera.fovy = 45.0f;                                // Camera field-of-view Y
   camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
   SetTargetFPS(60);                  
+  
+  //height map  
+  Image image = LoadImage("heightMap/heightMap.png");// Load heightmap image (RAM)
+  Texture2D texture = LoadTextureFromImage(image);// Convert image to texture (VRAM)
+  
+  //the levelPoints matrix: rows = 9, spacing = 6; therefore the mesh row and col is  9 * 6 = 54
+  Mesh levelMesh = GenMeshHeightmap(image, (Vector3){ 54, 1, 54 }); // Generate heightmap mesh (RAM and VRAM)
+  Model levelModel = LoadModelFromMesh(levelMesh);// Load model from generated mesh
+
+  levelModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture; // Set map diffuse texture
+  Vector3 levelModelPosition = { -30.0f, 0.0f, -30.0f };// Define model position
+
+  UnloadImage(image);// Unload heightmap image from RAM, already uploaded to VRAM 
     
   //initialise level points matrix
   struct Point levelPoints[ROWS][COLS];
-  initLevelPoints(levelPoints); 
-
+  Matrix transform = MatrixIdentity(); // Start with identity
+  transform = MatrixTranslate(levelModelPosition.x, levelModelPosition.y, levelModelPosition.z);
+  initLevelPoints(levelPoints, levelMesh, transform); 
+ 
   struct Cam cam;
   cam.cameraState = FIXED;
   
   struct EditMode editMode;
   editMode.editState = NULLSTATE;
-
+ 
     // Main game loop
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
@@ -47,19 +63,24 @@ int main(void)
         //render 3D goes here
         BeginMode3D(camera);
           renderLevel(levelPoints);
-          iterateLevelPoints(levelPoints);
+	  iterateLevelPoints(levelPoints);
+          DrawModel(levelModel, levelModelPosition, 1.0f, GREEN);
+
         EndMode3D();
 
         //render 2D goes here
-        
         //GUI
         renderButtons(&editMode);
 
       EndDrawing();
     }
   
-  // Close window and OpenGL context
+  //de-initialise
+  UnloadTexture(texture);     // Unload texture
+  UnloadModel(levelModel);
+  //Close window and OpenGL context
   CloseWindow();
+
 
   return 0;
 }
